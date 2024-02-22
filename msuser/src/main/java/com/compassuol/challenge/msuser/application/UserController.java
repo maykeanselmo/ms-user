601591b96bb2961cebfe67c952bd129503849ff6
Mayke <mayke000000000@gmail.com>
2024-02-22 10:15:17 -0300

    feat: Criação do UserController

---
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/UserController.java
@@ -0,0 +1,12 @@
+package com.compassuol.challenge.msuser.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/users")
+public class UserController {
+    private final UserService userService;
+}
