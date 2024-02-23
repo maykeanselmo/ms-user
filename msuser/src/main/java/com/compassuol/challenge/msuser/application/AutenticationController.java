3e7d4873451a3af0901d8230a6e822f38e76d67c
Mayke <mayke000000000@gmail.com>
2024-02-23 16:56:31 -0300

    feat: Controller de autenticação

---
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/AutenticationController.java
@@ -0,0 +1,49 @@
+package com.compassuol.challenge.msuser.application;
+
+import com.compassuol.challenge.msuser.application.dto.UserLoginDto;
+import com.compassuol.challenge.msuser.application.exceptions.handler.ErrorMessage;
+import com.compassuol.challenge.msuser.jwt.JwtToken;
+import com.compassuol.challenge.msuser.jwt.JwtUserDetailsService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+public class AutenticationController {
+    private final JwtUserDetailsService detailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UserLoginDto dto, HttpServletRequest request) {
+        log.info("Processo de autenticação pelo login {}", dto.getEmail());
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword());
+
+            authenticationManager.authenticate(authenticationToken);
+
+            JwtToken token = detailsService.getTokenAuthenticated(dto.getEmail());
+
+            return ResponseEntity.ok(token);
+        } catch (AuthenticationException ex) {
+            log.warn("Bad Credentials from username '{}'", dto.getEmail());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Credenciais Inválidas"));
+    }
+}
