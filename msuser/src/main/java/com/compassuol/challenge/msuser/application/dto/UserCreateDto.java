282adb9c8a47ecb3e1e9ca0c3710b6e49e0f9875
Mayke <mayke000000000@gmail.com>
2024-02-23 16:45:58 -0300

    feat: Validações

--- msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/UserCreateDto.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/UserCreateDto.java
@@ -1,13 +1,21 @@
 package com.compassuol.challenge.msuser.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserCreateDto {
     @NotEmpty
     @Column(name = "first_name", nullable = false)
@@ -19,16 +27,19 @@ public class UserCreateDto {
     private String lastName;
     @NotEmpty
     @Column(name = "cpf", nullable = false, unique = true)
-    @Pattern(regexp = "\\d{3}-\\d{3}-\\d{3}\\.\\d{2}", message = "CPF deve seguir o padrão xxx-xxx-xxx.xx")
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "CPF deve seguir o padrão xxx.xxx.xxx-xx")
     @NotEmpty
     private String cpf;
     @Column(name = "birthdate", nullable = false)
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthdate;
     @Column(name = "email", nullable = false, unique = true)
     @Email(message = "Formato inválido de email")
     @NotEmpty
     private String email;
     @Column(name = "cep", nullable = false)
+    @Pattern(regexp = "\\d{5}-\\d{3}", message = "CEP deve seguir o formato xxxxx-xxx")
     @NotEmpty
     private String cep;
     @Column(name = "password", nullable = false)
@@ -36,4 +47,7 @@ public class UserCreateDto {
     private String password;
     @Column(name = "active", nullable = false)
     private boolean active;
+    public boolean getActive() {
+        return active;
+    }
 }
