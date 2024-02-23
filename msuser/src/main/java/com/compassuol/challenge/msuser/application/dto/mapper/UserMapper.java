bc5c8822072ce823cfbe6333299c7bbe2e84e7c6
Mayke <mayke000000000@gmail.com>
2024-02-23 16:46:44 -0300

    feat: Criação de métodos no mapper

--- msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/mapper/UserMapper.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/mapper/UserMapper.java
@@ -1,19 +1,28 @@
 package com.compassuol.challenge.msuser.application.dto.mapper;
 
 import com.compassuol.challenge.msuser.application.dto.UserCreateDto;
+import com.compassuol.challenge.msuser.application.dto.UserLoginDto;
 import com.compassuol.challenge.msuser.application.dto.UserResponseDto;
-import com.compassuol.challenge.msuser.domain.User;
+import com.compassuol.challenge.msuser.application.dto.UserUpdatePasswordDto;
+import com.compassuol.challenge.msuser.domain.Usuario;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
-    public static UserResponseDto toDto(User user) {
+    public static UserResponseDto toDto(Usuario user) {
         return new ModelMapper().map(user, UserResponseDto.class);
     }
 
-    public static User toUser(UserCreateDto dto) {
-        return new ModelMapper().map(dto, User.class);
+    public static Usuario toUser(UserCreateDto dto) {
+        return new ModelMapper().map(dto, Usuario.class);
+    }
+    public  static UserUpdatePasswordDto toUserUpdatePasswordDto(Usuario user){
+        return new ModelMapper().map(user, UserUpdatePasswordDto.class);
+    }
+
+    public static UserLoginDto toUserLoginDto(Usuario user){
+        return new ModelMapper().map(user, UserLoginDto.class);
     }
 }
