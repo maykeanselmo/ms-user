a18a3e74f35994fba162d049db10d812924287e1
Mayke <mayke000000000@gmail.com>
2024-02-23 16:47:19 -0300

    feat: Adição de métodos de busca

--- msuser/src/main/java/com/compassuol/challenge/msuser/infra/repository/UserRepository.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/infra/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.compassuol.challenge.msuser.infra.repository;
 
-import com.compassuol.challenge.msuser.domain.User;
+import com.compassuol.challenge.msuser.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String email);
+
+
+    Usuario findByCpf(String cpf);
 }
