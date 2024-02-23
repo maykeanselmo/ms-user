06f6527c95de821556c5d360cea209a5dab0574b
Mayke <mayke000000000@gmail.com>
2024-02-23 16:48:02 -0300

    feat: Validação

--- msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/UserResponseDto.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/dto/UserResponseDto.java
@@ -1,11 +1,22 @@
 package com.compassuol.challenge.msuser.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserResponseDto {
     private String firstName;
     private String lastName;
     private String cpf;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate birthdate;
     private String email;
     private String cep;
