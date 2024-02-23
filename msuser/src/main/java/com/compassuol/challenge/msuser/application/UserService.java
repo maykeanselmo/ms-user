aa0462b7a648f2f0c8ee8a6ae81405acb41ea94d
Mayke <mayke000000000@gmail.com>
2024-02-23 16:48:59 -0300

    feat: Novos métodos de busca

--- msuser/src/main/java/com/compassuol/challenge/msuser/application/UserService.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/UserService.java
@@ -5,24 +5,25 @@ import com.compassuol.challenge.msuser.application.dto.UserLoginDto;
 import com.compassuol.challenge.msuser.application.dto.UserUpdatePasswordDto;
 import com.compassuol.challenge.msuser.application.exceptions.IncorrectPasswordExcpetion;
 import com.compassuol.challenge.msuser.application.exceptions.UniqueUserViolationException;
-import com.compassuol.challenge.msuser.domain.User;
+import com.compassuol.challenge.msuser.domain.Usuario;
 import com.compassuol.challenge.msuser.infra.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.sql.SQLIntegrityConstraintViolationException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public User createUser(User user) {
+    public Usuario createUser(Usuario user) {
         try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             return userRepository.save(user);
         }
         catch (DataIntegrityViolationException e){
@@ -33,15 +34,15 @@ public class UserService {
     }
 
     @Transactional(readOnly = true)
-    public User getUserById(Long id) {
+    public Usuario getUserById(Long id) {
         return userRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Nenhum usuário foi encontrado com este id: " + id)
         );
     }
 
     @Transactional
-    public User updateUser(Long id, UserCreateDto dto) {
-        User existingUser = getUserById(id);
+    public Usuario updateUser(Long id, UserCreateDto dto) {
+        Usuario existingUser = getUserById(id);
         existingUser.setFirstName(dto.getFirstName());
         existingUser.setLastName(dto.getLastName());
         existingUser.setEmail(dto.getEmail());
@@ -52,18 +53,18 @@ public class UserService {
         return createUser(existingUser);
     }
     @Transactional
-    public User updatePassword(Long id, String currentPassword, UserUpdatePasswordDto updatedPassword) {
-        User existingUser = getUserById(id);
-        if (existingUser.getPassword().equals(currentPassword)) {
-            existingUser.setPassword(updatedPassword.getPassword());
+    public Usuario updatePassword(Long id, String currentPassword, UserUpdatePasswordDto updatedPassword) {
+        Usuario existingUser = getUserById(id);
+        if (passwordEncoder.matches(currentPassword,existingUser.getPassword())) {
+            existingUser.setPassword(passwordEncoder.encode(updatedPassword.getPassword()));
             return userRepository.save(existingUser);
         } else
             throw new IncorrectPasswordExcpetion("Senha incorreta");
     }
     @Transactional
-    public User login(UserLoginDto dto) {
+    public Usuario login(UserLoginDto dto) {
 
-        User user = userRepository.findByEmail(dto.getEmail());
+        Usuario user = userRepository.findByEmail(dto.getEmail());
 
         if (user == null) {
             throw new EntityNotFoundException("Usuário não encontrado");
@@ -72,5 +73,14 @@ public class UserService {
             throw new IncorrectPasswordExcpetion("Senha incorreta.");
         }
         return user;
+    }
+    @Transactional(readOnly = true)
+    public Usuario getUserByEmail(String email) {
+        return  userRepository.findByEmail(email);
+    }
+    @Transactional(readOnly = true)
+    public Usuario findByCpf(String cpf) {
+        return userRepository.findByCpf(cpf);
+
     }
 }
