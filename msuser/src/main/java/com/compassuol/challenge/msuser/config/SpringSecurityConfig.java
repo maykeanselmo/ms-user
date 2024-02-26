ab702c69d3d7f822925272f6e8f8626d697e40d6
Mayke <mayke000000000@gmail.com>
2024-02-26 11:53:55 -0300

    feat: adição da dependência do swagger e sua configuração

--- msuser/src/main/java/com/compassuol/challenge/msuser/config/SpringSecurityConfig.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.compassuol.challenge.msuser.config;
 
+import com.compassuol.challenge.msuser.jwt.JwtAuthenticationEntryPoint;
 import com.compassuol.challenge.msuser.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,24 +20,42 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 public class SpringSecurityConfig {
+
+    private static final String[] DOCUMENTATION_OPENAPI = {
+            "/docs-ms-user.html",
+            "/docs-ms-user/**",
+            "/v3/api-docs/**",
+            "/swagger-ui-custom.html",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/**.html",
+            "/webjars/**",
+            "/configuration/**",
+            "/swagger-resources/**"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         http
-                .csrf(csrf->csrf.disable())
-                .formLogin(form-> form.disable())
+        http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/v1/users").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/v1/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/login").permitAll()
+                        .requestMatchers(DOCUMENTATION_OPENAPI).permitAll()
                         .anyRequest().authenticated())
-                 .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex-> ex
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
         return http.build();
 
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
@@ -44,6 +63,7 @@ public class SpringSecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
     @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
