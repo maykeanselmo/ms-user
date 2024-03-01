3fa61a9129a0d1aff7924e5908d903dba508146b
Mayke <mayke000000000@gmail.com>
2024-03-01 14:50:43 -0300

    feat: Habilitação do feign

--- msuser/src/main/java/com/compassuol/challenge/msuser/MsuserApplication.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/MsuserApplication.java
@@ -3,13 +3,16 @@ package com.compassuol.challenge.msuser;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableRabbit
+@EnableFeignClients
 @SpringBootApplication
 public class MsuserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsuserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsuserApplication.class, args);
+    }
+
 
 }
