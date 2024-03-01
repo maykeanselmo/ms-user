5ab39326f07afc375658bf636c9b5f8c6a8bce08
Mayke <mayke000000000@gmail.com>
2024-03-01 14:55:15 -0300

    feat: adição de exception

--- msuser/src/main/java/com/compassuol/challenge/msuser/application/exceptions/handler/ApiExceptionHandler.java
+++ msuser/src/main/java/com/compassuol/challenge/msuser/application/exceptions/handler/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.compassuol.challenge.msuser.application.exceptions.handler;
 
+import com.compassuol.challenge.msuser.application.exceptions.EmailNotFoundException;
+import com.compassuol.challenge.msuser.application.exceptions.ErrorNotificationException;
 import com.compassuol.challenge.msuser.application.exceptions.IncorrectPasswordExcpetion;
 import com.compassuol.challenge.msuser.application.exceptions.UniqueUserViolationException;
 import jakarta.persistence.EntityNotFoundException;
@@ -7,17 +9,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> handleGenericException() {
+    public ResponseEntity<ErrorMessage> handleGenericException(Exception e) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro inesperado."));
+                .body(new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()));
     }
     @ExceptionHandler(IncorrectPasswordExcpetion.class)
     public ResponseEntity<ErrorMessage> handIncorrectPasswordExcpetion(IncorrectPasswordExcpetion ex) {
@@ -34,11 +36,28 @@ public class ApiExceptionHandler {
                 .body(new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage()));
     }
     @ExceptionHandler(UniqueUserViolationException.class)
-    public ResponseEntity<ErrorMessage> handleUniqueProductViolationException(RuntimeException ex,
+    public ResponseEntity<ErrorMessage> handleUniqueUserViolationException(RuntimeException ex,
                                                                               HttpServletRequest request) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMessage(HttpStatus.CONFLICT, ex.getMessage()));
     }
+
+    @ExceptionHandler(EmailNotFoundException.class)
+    public ResponseEntity<ErrorMessage> handleEmailNotFoundException(EmailNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(ErrorNotificationException.class)
+    public ResponseEntity<ErrorMessage> handleErrorNotificationException(ErrorNotificationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR) // ou outro status de erro apropriado
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
+    }
+
 }
